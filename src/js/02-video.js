93916f58d161dd8951927d7ccfef4885fb1d322e
JuliaOraz <uliaoraz@gmail.com>
2022-07-20 05:43:35 +0300

    Добавляет 2 и 3 задание

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const VIDEOTIME_KEY = "videoplayer-current-time";
+
+// Сохраняем время в локальное хранилище
+player.on('timeupdate', throttle(setVideoPlayerTime,1000));
+
+function setVideoPlayerTime (currentTime) {
+    localStorage.setItem(VIDEOTIME_KEY, JSON.stringify(currentTime));
+}
+
+// Возобновляем воспроизведение с сохраненной позиции
+const getTime = localStorage.getItem(VIDEOTIME_KEY);
+
+if (getTime) { 
+    try {
+        const timeVideo = JSON.parse(getTime).seconds;       
+        player.setCurrentTime(timeVideo);
+        } catch (error) {
+            console.log("Ошибка"); 
+        }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+const formFeedback = document.querySelector('.feedback-form')
+const emailFeedback = formFeedback.querySelector('[name="email"]')
+const messageFeedback = formFeedback.querySelector('[name="message"]')
+
+const STORAGE_KEY = "feedback-form-state";
+let valueStorageFeedback = {};
+
+formFeedback.addEventListener('submit', onSubmitFeedback)
+formFeedback.addEventListener('input', throttle(onInputFeedback, 500))
+
+getDataForm()
+// Удаляет данные из локального хранилища и полей при отправке формы
+function onSubmitFeedback(e) {
+    e.preventDefault();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(`email: ${emailFeedback.value}\nmessage : ${messageFeedback.value}`)
+    e.currentTarget.reset()
+}
+ 
+
+// Сохраняет значения полей в локальное хранилище
+function onInputFeedback(e) {
+    valueStorageFeedback[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(valueStorageFeedback));
+}
+
+// Берет значения полей из локального хранилища
+function getDataForm() { 
+    const getValueFeedback = localStorage.getItem(STORAGE_KEY);
+    if (getValueFeedback) { 
+        try {
+            const getParseValue = JSON.parse(getValueFeedback);   
+            
+            valueStorageFeedback = getParseValue;
+            
+            emailFeedback.value  = getParseValue.email ? getParseValue.email : '';
+            messageFeedback.value = getParseValue.message ?  getParseValue.message : '';
+        } catch (error) {
+            console.log("Ошибка"); 
+        }
+    }
+    
+}
